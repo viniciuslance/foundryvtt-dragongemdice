d0b9e9da29e89c78f93e899c1766785dc909c88a
Vinicius Cupello <vigomes@firjan.com.br>
2021-06-22 00:23:49 -0300

    teste webp lokodemais

--- dragongem/scripts/dragongem.js
+++ dragongem/scripts/dragongem.js
@@ -3,49 +3,49 @@ Hooks.once('diceSoNiceReady', (dice3d) => {
 	dice3d.addDicePreset({
 		type: "d2",
 		labels: "",
-		modelFile: "modules/dragongem/models/wcube_d2.glb",
+		modelFile: "modules/dragongem/models/DragonGem_d2.gltf",
 		system: "dragongem"
 	});
 		dice3d.addDicePreset({
 		type: "d4",
 		labels: "",
-		modelFile: "modules/dragongem/models/wcube_d4.glb",
+		modelFile: "modules/dragongem/models/DragonGem_d4.gltf",
 		system: "dragongem"
 	});
 		dice3d.addDicePreset({
 		type: "d6",
 		labels: "",
-		modelFile: "modules/dragongem/models/DragonGem_d6.glb",
+		modelFile: "modules/dragongem/models/DragonGem_d6.gltf",
 		system: "dragongem"
 	});
 		dice3d.addDicePreset({
 		type: "d8",
 		labels: "",
-		modelFile: "modules/dragongem/models/wcube_d8.glb",
+		modelFile: "modules/dragongem/models/DragonGem_d8.gltf",
 		system: "dragongem"
 	});
 		dice3d.addDicePreset({
 		type: "d10",
 		labels: "",
-		modelFile: "modules/dragongem/models/wcube_d10.glb",
+		modelFile: "modules/dragongem/models/DragonGem_d10.gltf",
 		system: "dragongem"
 	});
 		dice3d.addDicePreset({
 		type: "d100",
 		labels: "",
-		modelFile: "modules/dragongem/models/wcube_d100.glb",
+		modelFile: "modules/dragongem/models/DragonGem_d100.gltf",
 		system: "dragongem"
 	});
 		dice3d.addDicePreset({
 		type: "d12",
 		labels: "",
-		modelFile: "modules/dragongem/models/wcube_d12.glb",
+		modelFile: "modules/dragongem/models/DragonGem_d12.gltf",
 		system: "dragongem"
 	});	
 		dice3d.addDicePreset({
 		type: "d20",
 		labels: "",
-		modelFile: "modules/dragongem/models/DragonGem_d20.glb",
+		modelFile: "modules/dragongem/models/DragonGem_d20.gltf",
 		system: "dragongem"
 	});
 });
